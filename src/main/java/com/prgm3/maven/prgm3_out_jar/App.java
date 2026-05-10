1cba88960936b3bc45325652dd4ce28f6d94a77c
venkatdanush <koda23csaiml@cmrit.ac.in>
2026-05-10 21:50:37 +0530

    initial commit

---
+++ src/main/java/com/prgm3/maven/prgm3_out_jar/App.java
@@ -0,0 +1,12 @@
+package com.prgm3.maven.prgm3_out_jar;
+
+/**
+ * Hello world!
+**/
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
